b5210a8f650807fc4e5e2ac75b54d60422b5e247
MAC <gabrielpelota.8@gmail.com>
2022-11-28 23:13:13 -0500

    fix bug

--- App.js
+++ App.js
@@ -137,20 +137,62 @@ function ControlsContainer({ join, leave, toggleWebcam, toggleMic }) {
   );
 }
 
-function ParticipantList() {
-  return null;
+function ParticipantView({ participantId }) {
+  const { webcamStream, webcamOn } = useParticipant(participantId);
+  
+  return webcamOn ? (
+    <RTCView
+      streamURL={new MediaStream([webcamStream.track]).toURL()}
+      objectFit={"cover"}
+      style={{
+        height: 300,
+        marginVertical: 8,
+        marginHorizontal: 8,
+      }}
+    />
+  ) : (
+    <View
+      style={{
+        backgroundColor: "grey",
+        height: 300,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 16 }}>NO MEDIA</Text>
+    </View>
+  );
+}
+
+function ParticipantList({ participants }) {
+  return participants.length > 0 ? (
+    <FlatList
+      data={participants}
+      renderItem={({ item }) => {
+        return <ParticipantView participantId={item} />;
+      }}
+    />
+  ) : (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#F6F6FF",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 20 }}>Press Join button to enter meeting.</Text>
+    </View>
+  );
 }
 function MeetingView() {
-  const { join, leave, toggleWebcam, toggleMic, meetingId } = useMeeting({});
+
+  const { join, leave, toggleWebcam, toggleMic, participants } = useMeeting({});
+  const participantsArrId = [...participants.keys()];
 
   return (
     <View style={{ flex: 1 }}>
-      {meetingId ? (
-        <Text style={{ fontSize: 18, padding: 12 }}>
-          Meeting Id :{meetingId}
-        </Text>
-      ) : null}
-      <ParticipantList /> // Will implement in next steps
+      <ParticipantList participants={participantsArrId} />
       <ControlsContainer
         join={join}
         leave={leave}
